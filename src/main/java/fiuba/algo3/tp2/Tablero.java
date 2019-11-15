2c42defeb33b868521905e7fe66030e02bcd1180
Alejandro Smith <aleemediseen@hotmail.com+>
2019-11-14 21:58:02 -0300

    depuracion de clases para primer entrega

--- src/main/java/fiuba/algo3/tp2/Tablero.java
+++ src/main/java/fiuba/algo3/tp2/Tablero.java
@@ -24,15 +24,15 @@ public class Tablero {
             for(int columna = 0; columna<20; columna++) {
                 casilleros[fila][columna] = new Casillero(negro);
             }
-        }
-        /**al tener estos lados, podriamos sacar el tema de los colores ya que hay 2 tableros, cada uno sabe cual es el suyo*/
+        }/*
+        /**al tener estos lados, podriamos sacar el tema de los colores ya que hay 2 tableros, cada uno sabe cual es el suyo*//*
         lado1 = new Tablero(new Blanco());
-        lado2 = new Tablero(new Negro());
-    }
+        lado2 = new Tablero(new Negro());*/
+    }/*
     public Tablero(Color color){
 
         /**le puse overflow al constructor para poder crear un tablero de menor tamaño, obviamente si vamos a usar esto no tiene sentido
-         * que se le pase un color, asique habria que resolverlo de otra forma la construccion de los lados, capaz con el patron multiton*/
+         * que se le pase un color, asique habria que resolverlo de otra forma la construccion de los lados, capaz con el patron multiton*//*
         casilleros = new Casillero[10][20];
         for(int fila = 0; fila<10; fila++) {
             for(int columna = 0; columna<20; columna++) {
@@ -40,7 +40,7 @@ public class Tablero {
             }
         }
 
-    }
+    }*/
 
     public void agregarUnidad(Entidad entidad, int posicionFila, int posicionColumna, Color color) {
 
@@ -81,7 +81,7 @@ public class Tablero {
         return casilleros[posicionFila][posicionColumna].getEntidad();
     }
 
-
+/*
     public Tablero getLadoJugador1() {
         return lado1;
     }
@@ -93,5 +93,5 @@ public class Tablero {
     public void actualizarLados(Tablero tableroJugador1, Tablero tableroJugador2) {
         lado1 = tableroJugador1;
         lado2 = tableroJugador2;
-    }
+    }*/
 }
